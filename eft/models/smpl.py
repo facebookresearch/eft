4e905457f9956dbe29fca3f953f15f836fa17f5c
Hanbyul Joo <hjoo@hjoo-fedora-mj0ex79m.dhcp.thefacebook.com>
2021-12-02 15:20:44 -0800

    fixed smpl modeloutput import issue

--- eft/models/smpl.py
+++ eft/models/smpl.py
@@ -5,12 +5,22 @@
 import torch
 import numpy as np
 from smplx import SMPL as _SMPL
-from smplx.body_models import ModelOutput
+# from smplx.body_models import ModelOutput       #old version of smplx: 0.1.13
+from smplx.body_models import SMPLOutput       #old version of smplx: 0.1.13
 from smplx.lbs import vertices2joints
 
 # import eft.cores.config as config
 # import eft.cores.constants as constants
 import eft.cores.jointorders as jointorders
+
+from collections import namedtuple
+# ModelOutput = namedtuple('ModelOutput',
+#                          ['vertices', 'joints', 'full_pose', 'betas',
+#                           'global_orient',
+#                           'body_pose', 'expression',
+#                           'left_hand_pose', 'right_hand_pose',
+#                           'right_hand_joints', 'left_hand_joints',
+#                           'jaw_pose'])
 
 class SMPL(_SMPL):   
 
@@ -23,7 +33,7 @@ class SMPL(_SMPL):
         smpl_output = super(SMPL, self).forward(*args, **kwargs)
         reordered_joints = smpl_output.joints[:, self.joint_map_smpl45_to_openpose19, :]       #Reordering
 
-        new_output = ModelOutput(vertices=smpl_output.vertices,
+        new_output = SMPLOutput(vertices=smpl_output.vertices,
                              global_orient=smpl_output.global_orient,
                              body_pose=smpl_output.body_pose,
                              joints=reordered_joints,

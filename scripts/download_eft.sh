23ee69a922e35c72c20b5c33014f98e2af7115d6
Hanbyul Joo <hjoo@fb.com>
2021-12-05 08:22:47 -0800

    minor update in scripts

--- scripts/download_eft.sh
+++ scripts/download_eft.sh
@@ -14,18 +14,17 @@ wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/COCO2014-All-ver01.json
 #COCO2014-Part ver0.1
 wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/COCO2014-Part-ver01.json
 
+#COCO2014-Val ver1.0 (manual filtering)
 wget https://dl.fbaipublicfiles.com/eft/COCO2014-Val-ver10.json
-
-
-
-# LSPet ver0.1 (outdated)
-# wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/LSPet_ver01.json
 
 #MPII ver0.1
 wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/MPII_ver01.json
 
 #PoseTrack ver0.1
 wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/PoseTrack_ver01.json
+
+# LSPet ver0.1 (outdated)
+# wget https://dl.fbaipublicfiles.com/eft/eft_fit_ver01/LSPet_ver01.json
 
 # LSPet ver1.0 (manual filtering)
 wget https://dl.fbaipublicfiles.com/eft/LSPet_test_ver10.json
